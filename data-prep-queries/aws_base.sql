462e7fd0423280e83ab84bb6caec01240220b705
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-01-29 23:11:02 -0800

    Update aws_base.sql

--- data-prep-queries/aws_base.sql
+++ data-prep-queries/aws_base.sql
@@ -1,64 +1,38 @@
+
 DROP TABLE IF EXISTS [aws_base];
 
 CREATE TABLE [aws_base]
   (
-   [mnth_date]             INT
-  ,[yr_date]               INT
-  ,[year_month_date]       DATETIME
-  ,[item_id]               VARCHAR(54)
-  ,[line_code]             VARCHAR(54)
-  ,[class_code]            VARCHAR(54)
-  ,[cc_type]               VARCHAR(54)
-  ,[pop_code_legacy]       VARCHAR(54)
-  ,[pop_code_new]          VARCHAR(54)
-  ,[finance_hierarchy]     VARCHAR(54)
-  ,[active_flag]           VARCHAR(54)
-  ,[Nov_keycust1]          VARCHAR(54)
-  ,[Oct_keycust1]          VARCHAR(54)
-  ,[Sep_keycust1]          VARCHAR(54)
-  ,[Aug_keycust1]          VARCHAR(54)
-  ,[Jul_keycust1]          VARCHAR(54)
-  ,[keycust1_high_level]   VARCHAR(54)
-  ,[keycust2_top_customer] VARCHAR(54)
-  ,[keycust3_territory]	   VARCHAR(54)
-  ,[keycust4]              VARCHAR(54)
-  ,[sales1]                VARCHAR(54)
-  ,[sales2]                VARCHAR(54)
-  ,[sales3]                VARCHAR(54)
-  ,[sales4]                VARCHAR(54)
-  ,[gross]                 NUMERIC(18, 2)
-  ,[qtyship]               INT
-  ,[qtyord]                INT
-  ,[qtyord_neg_rem]        INT
-  ,[qtyship_neg_rem]       INT
-  ,[frcst_qty]             INT
+   [year_month_date]          DATETIME
+  ,[mnth_date]                INT
+  ,[yr_date]                  INT
+  ,[item_id]                  VARCHAR(54)
+  ,[line_code]                VARCHAR(54)
+  ,[line_code_condensed]      VARCHAR(54)
+  ,[class_code]               VARCHAR(54)
+  ,[cc_type]                  VARCHAR(54)
+  ,[pop_code_legacy]          VARCHAR(54)
+  ,[top_customer_fb_w_perf]   VARCHAR(54)
+  ,[gross]                    NUMERIC(18, 2)
+  ,[qtyship]                  INT
+  ,[qtyord]                   INT
+  ,[qtyord_neg_rem]           INT
+  ,[qtyship_neg_rem]          INT
+  ,[frcst_qty]                INT
+  ,PRIMARY KEY ( [year_month_date] , [item_id], [top_customer_fb_w_perf] )
    ) ;
 
 WITH [cte_unconsolidated_dates] (
-    [mnth_date]
+    [year_month_date]
+   ,[mnth_date]
    ,[yr_date]
-   ,[year_month_date]
    ,[item_id]
    ,[line_code]
+   ,[line_code_condensed]
    ,[class_code]
    ,[cc_type]
    ,[pop_code_legacy]
-   ,[pop_code_new]
-   ,[finance_hierarchy]
-   ,[active_flag]
-   ,[Nov_keycust1]
-   ,[Oct_keycust1]
-   ,[Sep_keycust1]
-   ,[Aug_keycust1]
-   ,[Jul_keycust1]
-   ,[keycust1_high_level]
-   ,[keycust2_top_customer]
-   ,[keycust3_territory]
-   ,[keycust4]
-   ,[sales1]
-   ,[sales2]
-   ,[sales3]
-   ,[sales4]
+   ,[top_customer_fb_w_perf]
    ,[gross]
    ,[qtyship]
    ,[qtyord]
@@ -70,30 +44,19 @@ WITH [cte_unconsolidated_dates] (
   AS
   (
     SELECT 
-      DISTINCT MONTH([FrcstFactTbl].[InvDate])                         AS [mnth_date]
+      DISTINCT DATEADD(MONTH, DATEDIFF(MONTH, 0, [FrcstFactTbl].[InvDate]), 0)  AS [year_month]
+     ,MONTH([FrcstFactTbl].[InvDate])                                  AS [mnth_date]
      ,YEAR([FrcstFactTbl].[InvDate])                                   AS [yr]	
-     ,DATEADD(MONTH, DATEDIFF(MONTH, 0, [FrcstFactTbl].[InvDate]), 0)  AS [year_month]
      ,[FrcstFactTbl].[PartNumber]                                      AS [item_id]
-     ,[FrcstFactTbl].[LineCode]                                        AS [line_code]
-     ,[FrcstFactTbl].[ClassCode]                                       AS [class_code]
+     ,[ilu].[line_code]                                                AS [line_code]
+     ,[ilu].[line_code_condensed]                                      AS [line_code_condensed]
+     ,[ilu].[class_code]                                               AS [class_code]
      ,[ilu].[cc_type]                                                  AS [cc_type]
      ,[ilu].[pop_code]                                                 AS [pop_code_legacy]
-     ,[pc].[pop_code]                                                  AS [pop_code_new] 
-     ,[ah].[finance_hierarchy]                                         AS [finance_hierarchy]
-     ,[ah].[status]                                                    AS [active_flag]
-     ,[ah].[Nov_keycust1]                                              AS [Nov_keycust1]
-     ,[ah].[Oct_keycust1]                                              AS [Oct_keycust1]
-     ,[ah].[Sep_keycust1]                                              AS [Sep_keycust1]
-     ,[ah].[Aug_keycust1]                                              AS [Aug_keycust1] 
-     ,[ah].[Jul_keycust1]                                              AS [Jul_keycust1]
-     ,[ah].[keycust1_high_level]                                       AS [keycust1]
-     ,[ah].[keycust2_top_customer]                                     AS [keycust2]
-     ,[ah].[keycust3_territory]                                        AS [keycust3]
-     ,[ah].[keycust4]                                                  AS [keycust4]
-     ,[ah].[sales1]                                                    AS [sales1]
-     ,[ah].[sales2]                                                    AS [sales2]
-     ,[ah].[sales3]                                                    AS [sales3]
-     ,[ah].[sales4]                                                    AS [sales4]
+     ,CASE WHEN [ah].[top_customer_fb_w_perf] IS NULL
+           THEN 'All Other'
+           ELSE [ah].[top_customer_fb_w_perf]
+           END                                                         AS [top_customer_fb_w_perf]
      ,SUM([FrcstFactTbl].[GrossSales])                                 AS [Gross]
      ,SUM([FrcstFactTbl].[QtyShip])                                    AS [QtyShip]
      ,SUM([FrcstFactTbl].[QtyOrd])                                     AS [QtyOrd]
@@ -104,73 +67,47 @@ WITH [cte_unconsolidated_dates] (
      ( SUM([FrcstFactTbl].[QtyOrdNegRem]) 
         - 
        SUM([FrcstFactTbl].[QtyShipNegRem])
-     ) * 0.25                                                          AS [Forecast Qty]
+	   * 0.25
+     )                                                                 AS [Forecast Qty]
 	   
-			   
 
     FROM [FrcstFactTbl]
     LEFT JOIN [item_lu] ilu
-      ON [FrcstFactTbl].[PartNumber] = [ilu].[part_number]
+      ON [FrcstFactTbl].[PartNumber] = [ilu].[item_id]
     LEFT JOIN [account_hierarchy_lu_dec_20] ah 
       ON [FrcstFactTbl].[AccountNumber] = [ah].[AccountNumber]
     LEFT JOIN [pop_codes_12.4.2020] pc
       ON [FrcstFactTbl].[PartNumber] = [pc].[part_number]
-
+    WHERE [FrcstFactTbl].[PartNumber] IS NOT NULL
     GROUP BY
        MONTH([FrcstFactTbl].[InvDate])
       ,YEAR([FrcstFactTbl].[InvDate])
       ,DATEADD(MONTH, DATEDIFF(MONTH, 0, [FrcstFactTbl].[InvDate]), 0)   
       ,[FrcstFactTbl].[PartNumber]
-      ,[FrcstFactTbl].[LineCode]
-      ,[FrcstFactTbl].[ClassCode]
+      ,[ilu].[line_code]
+      ,[ilu].[line_code_condensed]
+      ,[ilu].[class_code] 
       ,[ilu].[cc_type]
       ,[ilu].[pop_code]
-      ,[pc].[pop_code]
-      ,[FrcstFactTbl].[AccountNumber]
-      ,[ah].[finance_hierarchy]
-      ,[ah].[status]
-      ,[ah].[Nov_keycust1]
-      ,[ah].[Oct_keycust1]
-      ,[ah].[Sep_keycust1]
-      ,[ah].[Aug_keycust1]
-      ,[ah].[Jul_keycust1]
-      ,[ah].[keycust1_high_level]
-      ,[ah].[keycust2_top_customer]
-      ,[ah].[keycust3_territory] 
-      ,[ah].[keycust4]
-      ,[ah].[sales1]	
-      ,[ah].[sales2]
-      ,[ah].[sales3]
-      ,[ah].[sales4] 
+      ,CASE WHEN [ah].[top_customer_fb_w_perf] IS NULL
+            THEN 'All Other'
+            ELSE [ah].[top_customer_fb_w_perf]
+            END
 		 
 )
 						 
 INSERT INTO [aws_base]
  (
-   [mnth_date]
+   [year_month_date]
+  ,[mnth_date]
   ,[yr_date]
-  ,[year_month_date]
   ,[item_id]
   ,[line_code]
+  ,[line_code_condensed]
   ,[class_code]
   ,[cc_type]
   ,[pop_code_legacy]
-  ,[pop_code_new]
-  ,[finance_hierarchy]
-  ,[active_flag]
-  ,[Nov_keycust1]
-  ,[Oct_keycust1]
-  ,[Sep_keycust1]
-  ,[Aug_keycust1]
-  ,[Jul_keycust1]
-  ,[keycust1_high_level]
-  ,[keycust2_top_customer]
-  ,[keycust3_territory]
-  ,[keycust4]
-  ,[sales1]
-  ,[sales2]
-  ,[sales3]
-  ,[sales4]
+  ,[top_customer_fb_w_perf]
   ,[gross]
   ,[qtyship]
   ,[qtyord]
@@ -179,30 +116,16 @@ INSERT INTO [aws_base]
   ,[frcst_qty]
 	)
 SELECT 
-   DISTINCT [mnth_date]
+   DISTINCT [year_month_date]
+  ,[mnth_date]
   ,[yr_date]
-  ,[year_month_date]
   ,[item_id]
   ,[line_code]
+  ,[line_code_condensed]
   ,[class_code]
   ,[cc_type]
   ,[pop_code_legacy]
-  ,[pop_code_new]
-  ,[finance_hierarchy]
-  ,[active_flag]
-  ,[Nov_keycust1]
-  ,[Oct_keycust1]
-  ,[Sep_keycust1]
-  ,[Aug_keycust1]
-  ,[Jul_keycust1]
-  ,[keycust1_high_level]
-  ,[keycust2_top_customer]
-  ,[keycust3_territory]
-  ,[keycust4]
-  ,[sales1]
-  ,[sales2]
-  ,[sales3]
-  ,[sales4]
+  ,[top_customer_fb_w_perf]
   ,SUM( [gross] )
   ,SUM( [qtyship] )
   ,SUM( [qtyord] )
@@ -213,28 +136,15 @@ SELECT
 FROM [cte_unconsolidated_dates]
 
 GROUP BY
-   [mnth_date]
+   [year_month_date]
+  ,[mnth_date]
   ,[yr_date]
   ,[year_month_date]
   ,[item_id]
   ,[line_code]
+  ,[line_code_condensed]
   ,[class_code]
   ,[cc_type]
   ,[pop_code_legacy]
-  ,[pop_code_new]
-  ,[finance_hierarchy]
-  ,[active_flag]
-  ,[Nov_keycust1]
-  ,[Oct_keycust1]
-  ,[Sep_keycust1]
-  ,[Aug_keycust1]
-  ,[Jul_keycust1]
-  ,[keycust1_high_level]
-  ,[keycust2_top_customer]
-  ,[keycust3_territory]
-  ,[keycust4]
-  ,[sales1]
-  ,[sales2]
-  ,[sales3]
-  ,[sales4]
-		 
+  ,[top_customer_fb_w_perf]
+
